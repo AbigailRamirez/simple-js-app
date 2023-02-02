8632155b3b1b1cf5be32cd6061a1520e9778d3c1
AbigailRamirez <ramirezabigail01@gmail.com>
2023-02-01 20:10:17 -0500

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-alert('Hello world');
-
-var favoriteFood='Miso Soup';
-
-document.write(favoriteFood);
+pokemonList = [
+    {name: 'pikachu', height: 40.64, types: ['electric']},
+    {name: 'bulbasaur', height: 71.12, types: ['grass', 'poison'] },
+    {name: 'charizard', height: 60.96, types: ['fire']},
+    {name: 'squirtle', height: 50.8, types: ['water']}
+];
