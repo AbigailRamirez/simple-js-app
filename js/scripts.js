b6434b7fee7a3c781f0f76e7dd87a5a2fe55d906
AbigailRamirez <ramirezabigail01@gmail.com>
2023-02-03 16:07:27 -0500

    Restructure & added For loop +conditional
    for loop prints out pokemon items and their height
    & conditional shows which one is big

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
 pokemonList = [
-    {name: 'pikachu', height: 40.64, types: ['electric']},
-    {name: 'bulbasaur', height: 71.12, types: ['grass', 'poison'] },
-    {name: 'charizard', height: 60.96, types: ['fire']},
-    {name: 'squirtle', height: 50.8, types: ['water']}
+    {
+        name: 'pikachu', 
+        height: 40.64, 
+        types: ['electric']
+    },
+    {
+        name: 'bulbasaur', 
+        height: 71.12, 
+        types: ['grass', 'poison'] 
+    },
+    {
+        name: 'charizard', 
+        height: 60.96, 
+        types: ['fire']
+    },
+    {
+        name: 'squirtle', 
+        height: 50.8, 
+        types: ['water']
+    }
 ];
+
+let pokedex = "";
+for (i=0; pokemonList[i]; i++){
+    pokedexReg= `${pokedex} ${pokemonList[i].name} ( height: ${pokemonList[i].height}cm)<br><br> ` ;
+    pokedexBig= `${pokedex} ${pokemonList[i].name} ( height: ${pokemonList[i].height}cm) - Wow, that's big! <br><br> ` ;
+    if (pokemonList[i].height < 65.0){
+        document.write(pokedexReg);
+    }else {
+        document.write(pokedexBig);
+    }
+}
