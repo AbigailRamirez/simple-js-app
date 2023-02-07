e11ad66de6ab265ef7c17c1ca2a523e143f4cf94
AbigailRamirez <ramirezabigail01@gmail.com>
2023-02-07 13:30:15 -0500

    bug fix - actually made pokemon list available outside IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,22 @@
 let pokemonRepository = (function (){
     let pokemonList = [
         {
-            name: 'pikachu', 
+            name: 'Pikachu', 
             height: 40.64, 
             types: ['electric']
         },
         {
-            name: 'bulbasaur', 
+            name: 'Bulbasaur', 
             height: 71.12, 
             types: ['grass', 'poison'] 
         },
         {
-            name: 'charizard', 
+            name: 'Charizard', 
             height: 60.96, 
             types: ['fire']
         },
         {
-            name: 'squirtle', 
+            name: 'Squirtle', 
             height: 50.8, 
             types: ['water']
         }
@@ -53,16 +53,13 @@ for (i=0; pokemonList[i]; i++){
 
 // replacing for loop with forEach() loop
 
-pokemonList.forEach(function(pokemon){
-    if (pokemon.height >= 60){
-        document.write(pokemon.name + " (height: " + pokemon.height + "cm) - Wow, that's big!!!" + "<br>")
-    } else {
-        document.write(pokemon.name + " (height: " + pokemon.height + "cm)" + "<br>")
-    }
-});
 
 pokemonRepository.getALL().forEach(function(pokemon){
-    document.write(pokemon.name + pokemon.height);
+    if (pokemon.height >= 60){
+        document.write(pokemon.name + " (height: " + pokemon.height + "cm) - Wow, that's big!!!" + "<br><br>")
+    } else {
+        document.write(pokemon.name + " (height: " + pokemon.height + "cm)" + "<br><br>")
+    }
 });
 
 
