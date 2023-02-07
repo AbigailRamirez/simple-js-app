3d4b143d9a879b40a7acde05a60d44da20161946
AbigailRamirez <ramirezabigail01@gmail.com>
2023-02-07 13:12:28 -0500

    Updated script with forEach & IIFE
    - forEach loop to iterate over pokemon list
    - created pokemon repository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,46 @@
-pokemonList = [
-    {
-        name: 'pikachu', 
-        height: 40.64, 
-        types: ['electric']
-    },
-    {
-        name: 'bulbasaur', 
-        height: 71.12, 
-        types: ['grass', 'poison'] 
-    },
-    {
-        name: 'charizard', 
-        height: 60.96, 
-        types: ['fire']
-    },
-    {
-        name: 'squirtle', 
-        height: 50.8, 
-        types: ['water']
-    }
-];
 
+
+
+let pokemonRepository = (function (){
+    let pokemonList = [
+        {
+            name: 'pikachu', 
+            height: 40.64, 
+            types: ['electric']
+        },
+        {
+            name: 'bulbasaur', 
+            height: 71.12, 
+            types: ['grass', 'poison'] 
+        },
+        {
+            name: 'charizard', 
+            height: 60.96, 
+            types: ['fire']
+        },
+        {
+            name: 'squirtle', 
+            height: 50.8, 
+            types: ['water']
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getALL(){
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getALL: getALL
+    };
+})();
+
+//before forEach() loop
+/*
 let pokedex = "";
 for (i=0; pokemonList[i]; i++){
     pokedexReg= `${pokedex} ${pokemonList[i].name} ( height: ${pokemonList[i].height}cm)<br><br> ` ;
@@ -29,5 +49,20 @@ for (i=0; pokemonList[i]; i++){
         document.write(pokedexReg);
     }else {
         document.write(pokedexBig);
+ */
+
+// replacing for loop with forEach() loop
+
+pokemonList.forEach(function(pokemon){
+    if (pokemon.height >= 60){
+        document.write(pokemon.name + " (height: " + pokemon.height + "cm) - Wow, that's big!!!" + "<br>")
+    } else {
+        document.write(pokemon.name + " (height: " + pokemon.height + "cm)" + "<br>")
     }
-}
+});
+
+pokemonRepository.forEach(function(pokemon){
+    document.write(pokemon.name + pokemon.height);
+});
+
+
